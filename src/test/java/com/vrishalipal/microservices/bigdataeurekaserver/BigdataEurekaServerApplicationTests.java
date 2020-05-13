ef597ef886d6c36d4ce155241d2ea85d79a7ebf6
vjpal3 <vrishalipal@yahoo.com>
2020-05-13 15:44:21 -0400

    Setup Netflix Eureka Server

---
+++ src/test/java/com/vrishalipal/microservices/bigdataeurekaserver/BigdataEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrishalipal.microservices.bigdataeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdataEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
